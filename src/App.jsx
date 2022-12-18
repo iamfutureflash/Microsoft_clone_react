337614b8242d3c347ab748f8208ec5b4d2636292
iamfutureflash <xenode2000@gmail.com>
2022-12-18 12:07:29 +0530

    deploy commit successfully

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={require('./vite.svg').default} className="logo" alt="Vite logo" />
+          <img src="./vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={require('./react.svg').default} className="logo react" alt="React logo" />
+          {/* <img src={require('./react.svg').default} className="logo react" alt="React logo" /> */}
+          <img src="./react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + puneet</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
